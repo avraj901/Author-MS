a27a7ae7a4fe604db0cdf70cc80233859707c422
unknown <cogjava3115@iiht.tech>
2022-08-17 16:06:14 +0530

    Added get by Id functionalities

--- src/main/java/com/author/controller/AuthorController.java
+++ src/main/java/com/author/controller/AuthorController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,7 +32,7 @@ public class AuthorController {
 	}
 	
 	@GetMapping("getAuthor/{id}")
-	public Optional<Author> getAuthor(){
-		return null;
+	public Optional<Author> getAuthor(@PathVariable Integer id){
+		return authorService.getAuthorById(id);
 	}
 }
